5565c6348d25079f9f6e211db18a79dc0b762503
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-07-19 19:17:43 +0530

    TranSactional

--- src/main/java/net/digest/journalAPP/service/JournalEntryService.java
+++ src/main/java/net/digest/journalAPP/service/JournalEntryService.java
@@ -7,8 +7,11 @@ import net.digest.journalAPP.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.*;
 
+//controller ---> service ---> repository
 @Component
 public class JournalEntryService{
 
@@ -17,11 +20,17 @@ private JournalEntryRepository journalEntryRepository;
 @Autowired
 private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName){
-        User user =userService.findByUserName(userName);
-        JournalEntry save=journalEntryRepository.save(journalEntry);
-        user.getJournalEntries().add(save);
-        userService.saveEntry(user);
+        try{
+            User user =userService.findByUserName(userName);
+            JournalEntry save=journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(save);
+            userService.saveEntry(user);
+        } catch (Exception e) {
+            throw new RuntimeException("An error occurred while saving the entry.", e);
+        }
+
     }
     public void saveEntry(JournalEntry journalEntry){
         journalEntryRepository.save(journalEntry);
@@ -35,6 +44,7 @@ private UserService userService;
         return journalEntryRepository.findById(id);
     }
 
+    @Transactional
     public void deleteJournalEntryId(ObjectId id,String userName){
         User user=userService.findByUserName(userName);
         user.getJournalEntries().removeIf(x ->x.getId().equals(id));
@@ -42,6 +52,3 @@ private UserService userService;
         journalEntryRepository.deleteById(id);
     }
 }
-
-
-//controller ---> service ---> repository
