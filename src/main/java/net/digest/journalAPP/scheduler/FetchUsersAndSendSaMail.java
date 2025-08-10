9e2630da9f656ecb0f9319ba531353a8a0498983
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-08-10 20:38:42 +0530

    javamailsender

---
+++ src/main/java/net/digest/journalAPP/scheduler/FetchUsersAndSendSaMail.java
@@ -0,0 +1,50 @@
+//package net.digest.journalAPP.scheduler;
+//
+//
+//import net.digest.journalAPP.cache.AppCache;
+//import net.digest.journalAPP.entity.JournalEntry;
+//import net.digest.journalAPP.entity.User;
+//import net.digest.journalAPP.repository.UserRespositoryImpl;
+//import net.digest.journalAPP.service.EmailService;
+//import net.digest.journalAPP.service.SentimentAnalysisService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//import java.time.LocalDateTime;
+//import java.time.temporal.ChronoUnit;
+//import java.util.*;
+//import java.util.stream.Collectors;
+//
+//@Component
+//public class FetchUsersAndSendSaMail {
+//
+//    @Autowired
+//    private EmailService emailService;
+//
+//    @Autowired
+//    private UserRespositoryImpl userRespository;
+//
+//    @Autowired
+//    private SentimentAnalysisService sentimentAnalysisService;
+//
+//    @Autowired
+//    private AppCache appCache;
+//
+//    @Scheduled(cron = "0 0 9 * * SUN")
+//    public void fetchUserAndSendSaMail(){
+//        List<User> users = userRespository.getUserForSA();
+//        for (User user: users){
+//            List<JournalEntry> journalEntries = user.getJournalEntries();
+//            List<String> filteredEntries = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map( x -> x.getContent()).collect(Collectors.toList());
+//            String combinedContent = String.join(" ", filteredEntries);
+//            int sentiment = sentimentAnalysisService.getSentiment(combinedContent);
+//            emailService.sendEmail(user.getEmail(), "Sentiment for last 7 days",sentiment);
+//        }
+//    }
+//
+//    @Scheduled(cron = "0 0/10 * ? * *")
+//    public void clearCache(){
+//        appCache.init();
+//    }
+//}
---
+++ src/main/java/net/digest/journalAPP/service/SentimentAnalysisService.java
@@ -0,0 +1,9 @@
+//package net.digest.journalAPP.service;
+//
+//
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class SentimentAnalysisService {
+//
+//}
