898d8040e0742e165921370a8caf54597d48cd9e
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-08-11 21:01:50 +0530

    constructor injection instend of dependency injection

--- src/main/java/net/digest/journalAPP/scheduler/UserScheduler.java
+++ src/main/java/net/digest/journalAPP/scheduler/UserScheduler.java
@@ -18,19 +18,29 @@ import java.util.stream.Collectors;
 @Service
 public class UserScheduler {
 
-    @Autowired
-    private EmailService emailService;
+//    @Autowired
+//    private EmailService emailService;
+//
+//    @Autowired
+//    private UserRespositoryImpl userRespository;
+//
+//    @Autowired
+//    private AppCache appCache;
 
-    @Autowired
-    private UserRespositoryImpl userRespository;
+    private final EmailService emailService;
+    private final UserRespositoryImpl userRepository;
+    private final AppCache appCache;
 
-    @Autowired
-    private AppCache appCache;
+    public UserScheduler(EmailService emailService, UserRespositoryImpl userRepository, AppCache appCache) {
+        this.emailService = emailService;
+        this.userRepository = userRepository;
+        this.appCache = appCache;
+    }
 
     @Scheduled(cron = "0 0 9 * * SUN")
     //@Scheduled(cron = "0 * * ? * *")
     public void fetchUserAndSendSaMail(){
-        List<User> users = userRespository.getUserForSA();
+        List<User> users = userRepository.getUserForSA();
         for (User user: users){
             List<JournalEntry> journalEntries = user.getJournalEntries();
             List<Sentiment> sentiments = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x -> x.getSentiment()).collect(Collectors.toList());
