e7355ba44e898273703400949174a1fc7facb7d2
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-08-14 21:13:04 +0530

    add redis

---
+++ src/test/java/net/digest/journalAPP/service/RedisTest.java
@@ -0,0 +1,20 @@
+package net.digest.journalAPP.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+public class RedisTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    void testSendMail(){
+        //redisTemplate.opsForValue().set("email","soumya@gmail.com");
+        Object email = redisTemplate.opsForValue().get("email");
+        int a =1;
+    }
+}
