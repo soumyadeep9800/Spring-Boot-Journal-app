2c09f29e3a55d5566351fbccabe73cda855ee5bd
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-08-14 21:24:29 +0530

    add redis config

--- src/test/java/net/digest/journalAPP/service/RedisTest.java
+++ src/test/java/net/digest/journalAPP/service/RedisTest.java
@@ -1,5 +1,6 @@
 package net.digest.journalAPP.service;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,9 +12,10 @@ public class RedisTest {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    @Disabled
     @Test
     void testSendMail(){
-        //redisTemplate.opsForValue().set("email","soumya@gmail.com");
+        redisTemplate.opsForValue().set("email","soumya@gmail.com");
         Object email = redisTemplate.opsForValue().get("email");
         int a =1;
     }
