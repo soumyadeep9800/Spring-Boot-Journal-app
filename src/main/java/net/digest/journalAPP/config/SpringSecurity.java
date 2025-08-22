18beaceec4206255dfb0527ab411ad9828c8a6e5
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-08-22 12:38:11 +0530

    start jwt

--- src/main/java/net/digest/journalAPP/config/SpringSecurity.java
+++ src/main/java/net/digest/journalAPP/config/SpringSecurity.java
@@ -1,4 +1,5 @@
 package net.digest.journalAPP.config;
+
 import net.digest.journalAPP.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
