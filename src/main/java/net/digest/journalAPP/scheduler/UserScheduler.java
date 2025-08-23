c632e9c8f9460947793faf0fd5db8d0099414b1b
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-08-23 09:31:36 +0530

    kafka fallback

--- src/main/java/net/digest/journalAPP/scheduler/UserScheduler.java
+++ src/main/java/net/digest/journalAPP/scheduler/UserScheduler.java
@@ -65,7 +65,11 @@ public class UserScheduler {
             }
             if(mostFrequentSentiment != null){
                 SentimentData sentimentData = SentimentData.builder().email(user.getEmail()).sentiment("Sentiment for last 7 days " + mostFrequentSentiment).build();
-                kafkaTemplate.send("sentiment", sentimentData.getEmail(), sentimentData);
+                try{
+                    kafkaTemplate.send("sentiment", sentimentData.getEmail(), sentimentData);
+                } catch (Exception e) {
+                    emailService.sendEmail(sentimentData.getEmail(), "Sentiment for previous week", sentimentData.getSentiment());
+                }
             }
         }
     }
